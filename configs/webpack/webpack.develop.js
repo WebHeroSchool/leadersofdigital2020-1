6bd62566899e6f47e9b623647fb5f116b617f21a
Pavel <tretyakovpavel.ru@gmail.com>
2019-08-07 16:10:13 +0300

    Required react-hotloader to react branch

--- configs/webpack/webpack.develop.js
+++ configs/webpack/webpack.develop.js
@@ -1,5 +1,15 @@
+const root = require('../helpers/root');
+
 module.exports = {
+  mode: 'development',
+
+  entry: [
+    'react-hot-loader/patch',
+    root(['src', 'index.jsx']),
+  ],
+
   devServer: {
+    hot: true,
     port: 9000,
   },
 };
--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { hot } from 'react-hot-loader/root';
+
+import Home from './home/home';
 
 import bagImage from 'assets/images/bag.png'
 
-export default () => (
+const App = () => (
   <div>
     <p>
       Bag React application
     </p>
 
     <img src={bagImage} alt="Bag Logo" />
+
+    <Home />
   </div>
 );
+
+export default hot(App);
