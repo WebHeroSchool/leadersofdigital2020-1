57f2fa6d4ba2b82ef4e93bb2b8cb7d4a853cadf5
Pavel Tretyakov <tretyakovpavel.ru@gmail.com>
2020-06-06 13:54:09 +0300

    Many changes and demo component

--- src/app/components/components.component.jsx
+++ src/app/components/components.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Button } from './shared/button/button';
+import { DemoComponent } from './shared/demo-component/demo-component';
 
 export const Components = () => {
     return (
@@ -10,6 +11,10 @@ export const Components = () => {
             <div>
                 <p>Компонента кнопки:</p>
                 <Button></Button>
+            </div>
+
+            <div style={{ marginTop: 13, border: '1px dashed #c6c6c6' }}>
+                <DemoComponent></DemoComponent>
             </div>
         </div>
     );
