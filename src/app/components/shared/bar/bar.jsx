5426106bcb6f8ffbf5e49360faa70fa73f4811ae
Veronika <nesteryuk15@gmail.com>
2020-06-06 17:41:58 +0300

    components

--- src/app/components/shared/bar/bar.jsx
+++ src/app/components/shared/bar/bar.jsx
@@ -1,16 +1,7 @@
 import React from "react";
 import "./bar.scss";
 
-<<<<<<< HEAD
-// const color = [
-//   #7E0202,
-//   #06895A,
-//   #0097AC,
-//   #B3A20A,
-//   #8F0056,
-//   #E48C08
-// ];
-=======
+
 const color = [
   "#7E0202",
   "#06895A",
@@ -19,10 +10,10 @@ const color = [
   "#8F0056",
   "#E48C08"
 ];
->>>>>>> f4a5aa3ce395a26857acf24f35c90531aa9c7730
+
 
 export const Bar = () => {
   return (
     <div className="ui-bar">Права, обязанности и ответственность</div>
   );
-};
+}
--- src/app/components/shared/contact/contact.jsx
+++ src/app/components/shared/contact/contact.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./contact.scss";
-import imgProfile from './img/profile.png';
+import imgProfile from './img/profile.svg';
 import imgMail from './img/mail.png';
 import imgTicks from './img/ticks.png';
 
 export const Contact = () => {
     return (
         <div className="ui-contact">
-            <div className="ui-img">
+          <div className="ui-img">
                 <img className="ui-contact-imagine__img" src={imgProfile} alt="profile"/>
-            </div>
+          </div>
             <div className="ui-block">
                 <div className="ui-name">
                 Иванов Степан Сидорович
@@ -17,12 +17,14 @@ export const Contact = () => {
                 <div className="ui-status">
                 Генеральный директор
                 </div>
+                <div className="ui-flex">
                 <div className="ui-mail">
                     <img className="ui-contact-mail__img" src={imgMail} alt="mail"/>
                     <a href="mailto:email@email.ru">email@email.ru</a>
                 </div>
                 <div className="ui-ticks">
                     <img className="ui-contact-ticks__img" src={imgTicks} alt="ticks"/>
+                </div>
                 </div>
             </div>
         </div>
