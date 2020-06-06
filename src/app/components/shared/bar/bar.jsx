f4a5aa3ce395a26857acf24f35c90531aa9c7730
Veronika <nesteryuk15@gmail.com>
2020-06-06 15:13:30 +0300

    components

--- src/app/components/shared/bar/bar.jsx
+++ src/app/components/shared/bar/bar.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import "./bar.scss";
 
 const color = [
-  #7E0202,
-  #06895A,
-  #0097AC,
-  #B3A20A,
-  #8F0056,
-  #E48C08
+  "#7E0202",
+  "#06895A",
+  "#0097AC",
+  "#B3A20A",
+  "#8F0056",
+  "#E48C08"
 ];
 
 export const Bar = () => {
