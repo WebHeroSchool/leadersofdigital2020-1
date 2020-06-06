477a6dd7ae37d75c7ecfcbbd057a78765553199e
Pavel Tretyakov <tretyakovpavel.ru@gmail.com>
2020-06-06 03:32:30 +0300

    Created button component

---
+++ src/app/components/shared/button/button.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './button.scss';
+
+export const Button = () => {
+    return (
+        <div className="ui-button">
+            Развернуть кнопку
+        </div>
+    );
+};
